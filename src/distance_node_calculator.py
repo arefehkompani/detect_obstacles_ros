79f166576c63bb3d223e8bf4ef195897b0009267
arefehkompani <awriik00@gmail.com>
2022-06-22 13:01:16 +0430

    Add list of names in get_distance and work with index

--- src/distance_node_calculator.py
+++ src/distance_node_calculator.py
@@ -4,6 +4,7 @@ import rospy
 import math
 from calculate_distance.srv import GetDistance, GetDistanceResponse
 from nav_msgs.msg import Odometry
+from sensor_msgs.msg import LaserScan
 
 class Distance_calculator:
     def __init__(self) -> None:
@@ -18,10 +19,14 @@ class Distance_calculator:
             ('fountain', (4.08, 1.14))
         ]
         
-    def calculate_distance(self, obstacle):
+    def calculate_distance(self, index):
         msg = rospy.wait_for_message("/odom", Odometry)
         position = msg.pose.pose.position
-        distance = math.sqrt((abs(obstacle[1][0] - position.x)**2 + abs(obstacle[1][1] - position.y)**2))
+
+        obstacle = self.obstacles[index]
+        obstacle_name, obstacle_pos = obstacle
+        distance = math.sqrt((abs(obstacle_pos[0] - position.x)**2 + abs(obstacle_pos[1] - position.y)**2))
+        rospy.loginfo(distance)
         return distance
 
     def get_distance(self, req):
@@ -29,21 +34,24 @@ class Distance_calculator:
         rospy.loginfo(f"NEW CALL: {ob_name}")
         distance = -1
 
-        if ob_name in self.obstacles:
-            distance = self.calculate_distance()
+        obstacle_names = list(map(lambda x : x[0], self.obstacles))
+
+        if ob_name in obstacle_names:
+            index = obstacle_names.index(ob_name)
+            rospy.loginfo("here")
+            distance = self.calculate_distance(index)
         else:
             rospy.logerr(f'ob_name is not valid: {ob_name}')
         
         res = GetDistanceResponse()
         res.distance = distance
-
         return res        
         
 def listener():
     rospy.init_node('distance_node_calculator', anonymous=True)
     dc = Distance_calculator()
-    rospy.Subscriber("/scan", LaserScan, dc.read_distance)
-    s = rospy.service('/get_distance', GetDistance, dc.get_distance)
+    #rospy.Subscriber("/scan", LaserScan, dc.read_distance)
+    s = rospy.Service('/get_distance', GetDistance, dc.get_distance)
     rospy.spin()
 
 if __name__=='__main__':
